f4e766c3e4e8ec196a5fcca8721e5c9b9dacf392
Lizzie <lizz.bo.bizz@gmail.com>
2018-02-25 20:02:09 +0000

    Added inactivity interrupts

--- src/adxl345.py
+++ src/adxl345.py
@@ -15,6 +15,8 @@ FIFO_CTL = 0x38         # FIFO control                      (r/w)
 FIFO_STATUS = 0x39      # FIFO status                       (r)
 
 THRESH_ACT = 0x24       # Activity threshold                (r/w)
+THRESH_INACT = 0x25     # Inactivity threshold              (r/w)
+TIME_INACT = 0x26       # Inactivity time                   (r/w)
 ACT_INACT_CTL = 0x27    # Axis enable for activity int      (r/w)
 
 DATAX0 = 0x32           # x-axis data 0                     (r)
@@ -27,14 +29,17 @@ DATAZ1 = 0x37           # z-axis data 1                     (r)
 # Register values
 MEASURE = 0x08          # [D3] = 1'b1 in POWER_CTL
 
-ACTIVITY = 0x10         # [D4] = 1'b1 in INT_ENABLE
+ACTIVITY = 0x10         # [D4] = 1'b1 in INT_ENABLE/INT_MAP
+INACTIVITY = 0x08       # [D3] = 1'b1 in INT_ENABLE/INT_MAP
 
 FIFO_MODE = 0x80        # [D7:D6] = 2'b10 in FIFO_CTL
 TRIGGER = 0x00          # [D5] = 1'b0 in FIFO_CTL
 SAMPLES = 0x00          # [D4:D0] = 5'b00001 in FIFO_CTL
 
-THRESHOLD = 0x20        # [D7:D0] = 8'd8 in THRESH_ACT
-ACT_CONFIG = 0x70       # [D7:D4] = 4'b0111 in ACT_INACT_CTL
+ACT_THRESHOLD = 0x20    # [D7:D0] = 8'd8 in THRESH_ACT
+INACT_THRESHOLD = 0x20  # [D7:D0] = 8'd8 in THRESH_INACT
+INACT_TIME = 0x05       # [D7:D0] = 8'd5 in INACT_TIME
+ACT_CONFIG = 0x77       # [D7:D0] = 8'h77 in ACT_INACT_CTL
 
 
 class ADXL345:
@@ -42,13 +47,15 @@ class ADXL345:
 
     def __init__(self, addr=0x53):
         self.addr = addr
-        self.config_act()
+        self.config_act_inact()
         self.config_fifo()
         self.config_int()
 
-    def config_act(self):
+    def config_act_inact(self):
         # Configure activity interrupts for specified threshold and axes
-        bus.write_byte_data(self.addr, THRESH_ACT, THRESHOLD)
+        bus.write_byte_data(self.addr, THRESH_ACT, ACT_THRESHOLD)
+        bus.write_byte_data(self.addr, THRESH_INACT, INACT_THRESHOLD)
+        bus.write_byte_data(self.addr, TIME_INACT, INACT_TIME)
         bus.write_byte_data(self.addr, ACT_INACT_CTL, ACT_CONFIG)
 
     def config_fifo(self):
@@ -57,8 +64,8 @@ class ADXL345:
 
     def config_int(self):
         # Enable interrupts triggered by ACTIVITY and send them to INT1
-        bus.write_byte_data(self.addr, INT_MAP, 0x00)
-        bus.write_byte_data(self.addr, INT_ENABLE, ACTIVITY)
+        bus.write_byte_data(self.addr, INT_MAP, ACTIVITY | INACTIVITY)
+        bus.write_byte_data(self.addr, INT_ENABLE, ACTIVITY | INACTIVITY)
 
     def run(self):
         # Enable measurement mode
