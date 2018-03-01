605e20b1b669293340ece2d308d03d5061f4469d
Lizzie <lizz.bo.bizz@gmail.com>
2018-03-01 22:01:45 +0000

    Program configured to detect tampering using ACTIVITY interrupts. Sensitivity can be configured using parameters defined in app.py.

--- src/adxl345.py
+++ src/adxl345.py
@@ -4,6 +4,8 @@ import smbus
 # Get bus for I2C
 bus = smbus.SMBus(1)
 
+########################################################################
+
 # Register addresses
 POWER_CTL = 0x2D        # Power-saving features control     (r/w)
 
@@ -15,57 +17,44 @@ FIFO_CTL = 0x38         # FIFO control                      (r/w)
 FIFO_STATUS = 0x39      # FIFO status                       (r)
 
 THRESH_ACT = 0x24       # Activity threshold                (r/w)
-THRESH_INACT = 0x25     # Inactivity threshold              (r/w)
-TIME_INACT = 0x26       # Inactivity time                   (r/w)
 ACT_INACT_CTL = 0x27    # Axis enable for activity int      (r/w)
 
-DATAX0 = 0x32           # x-axis data 0                     (r)
-DATAX1 = 0x33           # x-axis data 1                     (r)
-DATAY0 = 0x34           # y-axis data 0                     (r)
-DATAY1 = 0x35           # y-axis data 1                     (r)
-DATAZ0 = 0x36           # z-axis data 0                     (r)
-DATAZ1 = 0x37           # z-axis data 1                     (r)
+########################################################################
 
 # Register values
 MEASURE = 0x08          # [D3] = 1'b1 in POWER_CTL
 
 ACTIVITY = 0x10         # [D4] = 1'b1 in INT_ENABLE/INT_MAP
-INACTIVITY = 0x08       # [D3] = 1'b1 in INT_ENABLE/INT_MAP
 
 FIFO_MODE = 0x80        # [D7:D6] = 2'b10 in FIFO_CTL
-TRIGGER = 0x00          # [D5] = 1'b0 in FIFO_CTL
-SAMPLES = 0x00          # [D4:D0] = 5'b00001 in FIFO_CTL
 
 ACT_THRESHOLD = 0x20    # [D7:D0] = 8'd8 in THRESH_ACT
-INACT_THRESHOLD = 0x20  # [D7:D0] = 8'd8 in THRESH_INACT
-INACT_TIME = 0x05       # [D7:D0] = 8'd5 in INACT_TIME
-ACT_CONFIG = 0x77       # [D7:D0] = 8'h77 in ACT_INACT_CTL
+ACT_CONFIG = 0x70       # [D7:D0] = 8'h70 in ACT_INACT_CTL
 
+########################################################################
 
 class ADXL345:
     addr = None
 
     def __init__(self, addr=0x53):
         self.addr = addr
-        self.config_act_inact()
+        self.config_act()
         self.config_fifo()
         self.config_int()
 
-    def config_act_inact(self):
+    def config_act(self):
         # Configure activity interrupts for specified threshold and axes
         bus.write_byte_data(self.addr, THRESH_ACT, ACT_THRESHOLD)
-        bus.write_byte_data(self.addr, THRESH_INACT, INACT_THRESHOLD)
-        bus.write_byte_data(self.addr, TIME_INACT, INACT_TIME)
         bus.write_byte_data(self.addr, ACT_INACT_CTL, ACT_CONFIG)
 
     def config_fifo(self):
-        # Configure FIFO for FIFO mode with interrupts on INT1 triggered by one FIFO entry
-        bus.write_byte_data(self.addr, FIFO_CTL, FIFO_MODE | TRIGGER | SAMPLES)
+        # Configure FIFO for stream mode
+        bus.write_byte_data(self.addr, FIFO_CTL, FIFO_MODE)
 
     def config_int(self):
         # Enable interrupts triggered by ACTIVITY and send them to INT1
-        bus.write_byte_data(self.addr, INT_MAP, ACTIVITY | INACTIVITY)
-        bus.write_byte_data(self.addr, INT_ENABLE, ACTIVITY | INACTIVITY)
+        bus.write_byte_data(self.addr, INT_MAP, 0x00)
+        bus.write_byte_data(self.addr, INT_ENABLE, ACTIVITY)
 
     def run(self):
         # Enable measurement mode
--- src/app.py
+++ src/app.py
@@ -4,6 +4,16 @@ import RPi.GPIO as GPIO
 from adxl345 import ADXL345
 from device_status import DeviceStatus
 
+# Total motion duration for suspicious motion (seconds)
+watch_total_duration = 3
+
+# Interrupt watch interval duration (seconds)
+watch_interval = 1
+
+# Consecutive number of interrupts to determine suspicious motion
+trig_int_threshold = watch_total_duration / watch_interval
+trig_int_count = 0
+
 # Set GPIO mode to use board numbering scheme
 GPIO.setmode(GPIO.BOARD)
 
@@ -21,29 +31,19 @@ device_status = DeviceStatus()
 adxl345 = ADXL345()
 adxl345.run()
 
-"""
 while True:
-    # Poll interrupt signal
-    if GPIO.input(INT1):
-        # Update device status (triggered)
-        DeviceStatus.triggered = True
-        # Trigger alarm signal
-
-        # Clear interrupt
+    # Wait for interrupt interval
+    time.sleep(watch_interval)
+    
+    print('Interrupt count = {}'.format(trig_int_count))
+    
+    if (GPIO.input(INT1)):
+        trig_int_count += 1
         adxl345.clear_int()
+    else:
+        trig_int_count = 0
+    
+    if (trig_int_count >= trig_int_threshold):
+        print('TAMPERING DETECTED')
+        trig_int_count = 0
 
-    # Poll data from app
-
-    # Update device status (battery)
-
-    time.sleep(0.5)
-"""
-# adxl345.clear_int()
-while True:
-    if GPIO.input(INT1):
-        print('INT1 is high')
-        adxl345.clear_int()
-    if GPIO.input(INT2):
-        print('INT2 is high')
-        adxl345.clear_int()
-    time.sleep(0.5)
